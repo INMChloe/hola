ac62f6eb7bc9a933bcf34a20a3803c88c2f401e0
INMChloe <chloenjeri21@gmail.com>
2024-11-28 10:51:00 +0300

    third commit

--- Myproject/UDID/views.py
+++ Myproject/UDID/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def Chloe (request):
+    return HttpResponse("<h1>WELCOME TO MY WEBSITE<h1>")
